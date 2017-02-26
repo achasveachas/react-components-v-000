327d4ea19f06ea52f402f254e5ae40be48e8fabc
Yechiel K <chilikkal@gmail.com>
2017-02-25 23:55:05 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const app = "I don't do much.";
+class Button extends React.Component {
+  render(){
+    return React.createElement('button', {}, 'Click me!')
+  }
+}
+
+const nestedButtons = React.createElement('div', {}, [
+  React.createElement(Button),
+  React.createElement(Button)
+])
+ReactDOM.render(nestedButtons, document.getElementById('main'))
